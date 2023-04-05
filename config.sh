543498e474d71df65a5cacfe7dbe96960bef275c
BartoszJ <jachimczyk.bartosz@gmail.com>
2023-04-06 00:04:47 +0200

    config.sh passed container test. Remianing polishing required like : help file and running R terminal at start

--- config.sh
+++ config.sh
@@ -4,9 +4,11 @@
 # of particular components, for debugging purpouses.
 
 Rinstall=true		#R 4.2 installlation
-Nviminstall=false	#Nvim 0.8 installation
-initconfig=false	#Nvim plugins configuration
+Nviminstall=true	#Nvim 0.8 installation
+initconfig=true	#Nvim plugins configuration
 Rlibs=true		#Installation of R libraries
+mininstall=true #Do not install plugins other than R related
+customopt=false # Assign custom options at the nvim start
 
 # 1. APT UPDATEs
 #
@@ -19,13 +21,13 @@ Rlibs=true		#Installation of R libraries
 apt-get update
 # Many popular UNIX tools are not preinstalled 
 # in debian container
+apt-get install dialog -y
+apt-get install apt-utils -y
 apt-get install curl -y
 apt-get install git -y
 apt-get install gpg -y
-apt-get install apt-utils -y
 # Nvimdependencies
-apt-get install dialog
-apt-get install libterm-readline-gnu-perl
+apt-get install libterm-readline-gnu-perl -y 
 # R extrnal dependencies
 # Needed for R libraries to work
 apt-get install cmake -y
@@ -44,9 +46,6 @@ apt-get install libtiff5-dev -y
 # R to the news version deb cran repo needs to be added to
 # apt sources with approriate key.
 #
-# BUG: Currently  container has a problem with adding
-# key for cran repo. It is possible to solve in running 
-# container, thought.
 if [ $Rinstall = true ]; then
     apt-get update
     apt-get install r-base r-base-dev -y
@@ -55,7 +54,7 @@ if [ $Rinstall = true ]; then
     # This step is needed to update R to 4.2
     # Add key for CRAN repo
     gpg --keyserver keyserver.ubuntu.com --recv-key '95C0FAF38DB3CCAD0C080A7BDC78B2DDEABC47B7'
-    gpg --armor --export '95C0FAF38DB3CCAD0C080A7BDC78B2DDEABC47B7' | tee /etc/apt/trusted.gpg.d/cran_debian_key.asc # THIS LINE HAD A BUG
+    gpg --armor --export '95C0FAF38DB3CCAD0C080A7BDC78B2DDEABC47B7' | tee /etc/apt/trusted.gpg.d/cran_debian_key.asc
 
     echo "deb http://cloud.r-project.org/bin/linux/debian bullseye-cran40/" >> /etc/apt/sources.list
     apt-get update && apt-get upgrade -y
@@ -93,21 +92,34 @@ if [ $initconfig = true ]; then
     mkdir ~/.config ~/.config/nvim
     touch ~/.config/nvim/init.vim
     # Modifies init.vim global variables
+    #
+    # This lines allows nvim to find installed pluggins
+    # and R binaries.
+    #
     echo "let g:plugged_home = '~/.config/nvim'" >> ~/.config/nvim/init.vim
     echo "let g:R_path = '/usr/bin/R'" >> ~/.config/nvim/init.vim
     echo "let g:R_assign = 1" >> ~/.config/nvim/init.vim
     echo "let R_auto_omni = 0" >> ~/.config/nvim/init.vim
     echo "  " >> ~/.config/nvim/init.vim
-    # Installs plugins from nvim`
+    
+    # Lines below adds call plug, and all commands to
+    # install all plugins. Plugins were separated into
+    # one related to R and other(controled by $mininstall)
+    #
     echo "call plug#begin()" >> ~/.config/nvim/init.vim
-    echo "Plug 'https://github.com/junegunn/vim-github-dashboard.git'"  >> ~/.config/nvim/init.vim
-    echo "Plug 'SirVer/ultisnips' | Plug 'honza/vim-snippets'" >> ~/.config/nvim/init.vim
-    echo "Plug 'preservim/nerdtree', { 'on': 'NERDTreeToggle' }" >> ~/.config/nvim/init.vim
-    echo "Plug 'tpope/vim-fireplace', { 'for': 'clojure' }" >> ~/.config/nvim/init.vim
-    echo "Plug 'rdnetto/YCM-Generator', { 'branch': 'stable' }" >> ~/.config/nvim/init.vim
-    echo "Plug 'fatih/vim-go', { 'tag': '*' }" >> ~/.config/nvim/init.vim
-    echo "Plug 'nsf/gocode', { 'tag': 'v.20150303', 'rtp': 'vim' }" >> ~/.config/nvim/init.vim
-    echo "Plug 'junegunn/fzf', { 'dir': '~/.fzf', 'do': './install --all' }" >> ~/.config/nvim/init.vim
+
+    # mininstall=false - no additional plugins are instlalled
+    # with nvim, except for the one related to R
+    if [ $mininstall = false ]; then
+		echo "Plug 'https://github.com/junegunn/vim-github-dashboard.git'"  >> ~/.config/nvim/init.vim
+	echo "Plug 'SirVer/ultisnips' | Plug 'honza/vim-snippets'" >> ~/.config/nvim/init.vim
+	echo "Plug 'preservim/nerdtree', { 'on': 'NERDTreeToggle' }" >> ~/.config/nvim/init.vim
+	echo "Plug 'tpope/vim-fireplace', { 'for': 'clojure' }" >> ~/.config/nvim/init.vim
+	echo "Plug 'rdnetto/YCM-Generator', { 'branch': 'stable' }" >> ~/.config/nvim/init.vim
+	echo "Plug 'fatih/vim-go', { 'tag': '*' }" >> ~/.config/nvim/init.vim
+	echo "Plug 'nsf/gocode', { 'tag': 'v.20150303', 'rtp': 'vim' }" >> ~/.config/nvim/init.vim
+	echo "Plug 'junegunn/fzf', { 'dir': '~/.fzf', 'do': './install --all' }" >> ~/.config/nvim/init.vim
+    fi
 
     #R RELATED PLUGS:
     echo "Plug 'gaalcaras/ncm-R'" >> ~/.config/nvim/init.vim
@@ -117,12 +129,29 @@ if [ $initconfig = true ]; then
     echo "Plug 'jalvesaq/R-Vim-runtime'" >> ~/.config/nvim/init.vim
 
     echo "call plug#end()" >> ~/.config/nvim/init.vim
-    echo ':set number' >> ~/.config/nvim/init.vim
-    echo ':set relativenumber' >> ~/.config/nvim/init.vim
-    echo ':set clipboard+=unnamedplus "Yanking between windows' >> ~/.config/nvim/init.vimv
-    echo ':set shiftwidth=4 "Intendentation' >> ~/.config/nvim/init.vim
-    echo "PLUG INSTALL >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
-    nvim +PlugInstall +q
-    nvim test
+    # $customopt - changes starting options of nvim, such as:
+    # displaying relative line numbers and intendention width,
+    #
+    if [ $customopt = true ]; then
+	echo ':set number' >> ~/.config/nvim/init.vim
+	echo ':set relativenumber' >> ~/.config/nvim/init.vim
+	echo ':set clipboard+=unnamedplus "Yanking between windows' >> ~/.config/nvim/init.vimv
+	echo ':set shiftwidth=4 "Intendentation' >> ~/.config/nvim/init.vim
+	echo "PLUG INSTALL >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
+    fi
+    # $Rlibs - installs R packages
+    #
+    # A text file libinstall.R is created to be called
+    # with Rscript function. After package installation
+    # the file is removed.
+    #
+    if [ $Rlibs = true ]; then
+	touch libinstall.R
+	echo "install.packages('tidyverse')" >> libinstall.R
+	Rscript libinstall.R
+	rm libinstall.R
+    fi
 
+    nvim +PlugInstall +qall
+    nvim test
 fi
